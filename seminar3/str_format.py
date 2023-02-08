b6c007b6b17bb8f45569fa88515a7ed73cf799f7
Maks <fonyak_maks@mail.ru>
2023-02-08 10:47:27 +0500

    почистил папку семинар 3

--- seminar3/str_format.py
+++
@@ -1,9 +0,0 @@
-age = 40
-name = 'Max'
-lastname = 'Fon'
-print('Возраст {} {} -- {} лет.'.format(name, lastname, age))
-print('Почему {} {} забавляется с этим Python?'.format(name, lastname))
-
-
-print('{name} написал {book}'.format(name='Swaroop', book='A Byte of Python'))
-# 'Swaroop написал A Byte of Python'
