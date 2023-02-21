536f9671ab52c13db5365dac478c4e855572b245
Maks <fonyak_maks@mail.ru>
2023-02-21 14:38:22 +0500

    решил задачу 1 Seminar7

--- seminar7/zadacha7_1.py
+++ seminar7/zadacha7_1.py
@@ -12,3 +12,15 @@
 
 # Вывод:
 # Парам пам-пам
+
+def rifma(chant):
+    st = chant.lower().split()
+    f = lambda x: sum(1 for i in x if i in 'аеёиоуыэюя')
+    tmp = f(st[0])
+    if all([f(i) == tmp for i in st]):
+        return 'Парам пам-пам'
+    return 'Пам парам'
+ 
+print(rifma("паррра-ррра-рорррам рам-пам-папам па-ра-па-да"))
+
+ 
